af439111153fb08061d002711a23900acee2c6fa
yuta.dammatsu <yuta.dammatsu@gmail.com>
2021-01-30 22:30:23 +0900

    Home 画面を作成

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,141 @@
-const Home: React.FC = () => {
-  return(
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    menuButton: {
+      textAlign: 'right'
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  }),
+);
+
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+}
+
+const Home: React.FC<Props> = (props) => {
+  const { window } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
     <div>
-      ホーム画面
+      <List>
+        <ListItem style={{color: 'gray', justifyContent: 'flex-end'}}>
+          <CloseIcon onClick={() => {handleDrawerToggle();}}/>
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon><ExitToAppIcon /></ListItemIcon>
+          <ListItemText primary="ログアウト" />
+        </ListItem>
+      <ListItem button>
+          <ListItemIcon>< AccountCircleIcon /></ListItemIcon>
+          <ListItemText primary="プロフィール" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon><MailIcon /></ListItemIcon>
+          <ListItemText primary="メッセージ" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>< BookmarkBorderIcon /></ListItemIcon>
+          <ListItemText primary="保存記事" />
+        </ListItem>
+      </List>
     </div>
   );
-};
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" style={{backgroundColor: 'black'}}>
+        <Toolbar style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Typography variant="h6" noWrap>
+              Session!
+            </Typography>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+            >
+            <MenuIcon className={classes.menuButton}/>
+            
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <nav aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor='right'
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <div>
+          </div>
+        <Typography>
+          Session! はミュージシャンのためのマッチングアプリです。<br/>
+          一緒に音楽をしたいと思える仲間を見つけ、音楽を楽しみましょう。
+        </Typography>
+      </main>
+    </div>
+  );
+}
 
 export default Home;
